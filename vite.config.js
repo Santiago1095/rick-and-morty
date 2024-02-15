2b200a4a4084dd3a0f9b11ab293bb2feb2ae351b
Santiago Montañez <santiago.mbaron@gmail.com>
2024-02-14 20:31:29 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:"https:/santiago1095.github.io/rick-and-morty"
+})
